d2de0d15cc6f331886b174f540547e32feb2d3dc
Nicole <41876584+NicoleFaye@users.noreply.github.com>
2025-06-16 21:47:13 -0700

    take 1 pokemon pinball pufferlib addition

---
+++ pufferlib/environments/pokemon_pinball/environment.py
@@ -0,0 +1,30 @@
+
+import gymnasium
+import functools
+
+from pokemon_pinball_gym import PokemonPinballEnv
+
+import pufferlib.emulation
+import pufferlib.postprocess
+
+
+def env_creator(name='pokemon_pinball'):
+    return functools.partial(make, name)
+
+def make(name, headless: bool = True, state_path=None, buf=None):
+    '''Pokemon Pinball'''
+    env = PokemonPinballEnv(headless=headless, state_path=state_path)
+    env = RenderWrapper(env)
+    env = pufferlib.postprocess.EpisodeStats(env)
+    return pufferlib.emulation.GymnasiumPufferEnv(env=env, buf=buf)
+
+class RenderWrapper(gymnasium.Wrapper):
+    def __init__(self, env):
+        self.env = env
+
+    @property
+    def render_mode(self):
+        return 'rgb_array'
+
+    def render(self):
+        return self.env.screen.screen_ndarray()
